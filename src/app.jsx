fda4e2eecd342e30f0c5a7bf784912021f02a5d7
DoHyunHwang <dohyun033@gmail.com>
2021-07-08 16:06:56 +0900

    반응형 작업 및 axios

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
@@ -11,27 +11,31 @@ function App({ youtube }) {
   const selectVideo = (video) => {
     setSelectedVideo(video);
   }
-  const search = query => {
+  const search = useCallback(query => {
     youtube
       .search(query)
-      .then(videos => setVideos(videos));
-  }
+      .then(videos => {
+        setVideos(videos)
+        setSelectedVideo(null);
+      });
+  }, [])
 
   useEffect(() => {
     youtube
       .mostPopular()
-      .then(videos => setVideos(videos));
-      
-  }, []);
+      .then(videos => setVideos(videos));    
+  }, [youtube]);
   return (
     <div className = {styles.app}>
       <SearchHeader onSearch={search}/>
-      <section className={StyleSheet.content}>
+      <section className={styles.content}>
+      {selectedVideo && (
         <div className={styles.detail}>
-          {selectedVideo && <VideoDetail video={selectedVideo} />}
+          <VideoDetail video={selectedVideo} /> 
         </div>
+        )}
         <div className={styles.list}>
-          <VideoList videos={videos} onVideoClick={selectVideo} />
+          <VideoList videos={videos} onVideoClick={selectVideo} display = { selectedVideo? 'list' : 'grid' } />
         </div>
       </section>
     </div>
